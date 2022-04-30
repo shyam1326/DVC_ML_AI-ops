ba73d761dbf21fd5effbe4d47a00ea184763f2e8
shyam prasath <shyamprasath@detecttechnologies.com>
2022-04-30 15:42:56 +0530

    stage 01 create

---
+++ src/stage_01_load_save.py
@@ -0,0 +1,37 @@
+from src.utils.all_utils import read_yaml, create_directory
+import argparse
+import pandas as pd
+import os
+
+def get_data(config_path):
+    config = read_yaml(config_path)
+    data_path = config['data_source']
+    data = pd.read_csv(data_path, sep=';')
+
+    # Save the data in a local directory
+    # create path to artifacts directory : artifacts/raw_local_dir/data.cs
+    artifacts_dir = config['artifacts']['artifacts_dir']
+    raw_local_dir = config['artifacts']['raw_local_dir']
+    raw_local_file = config['artifacts']['raw_local_file']
+
+    raw_local_dir_path = os.path.join(artifacts_dir, raw_local_dir)
+
+    create_directory(dirs=[raw_local_dir_path])
+
+    raw_local_file_path = os.path.join(raw_local_dir_path, raw_local_file)
+    data.to_csv(raw_local_file_path, sep=';', index=False)
+
+
+
+
+
+
+if __name__ == "__main__":
+    args = argparse.ArgumentParser()
+
+    args.add_argument("--config", "-c", default="config/config.yaml")
+
+    parsed_args = args.parse_args()
+    # print(parsed_args.config)
+    get_data(config_path= parsed_args.config)
+ 
--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -6,3 +6,8 @@ def read_yaml(path : str) -> dict:
         content = yaml.safe_load(yaml_file)
     
     return content
+
+def create_directory(dirs : list):
+    for dir_path in dirs:
+        os.makedirs(dir_path, exist_ok=True)
+        print(f"Created directory {dir_path}")
